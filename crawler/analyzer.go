65288e257bb7c02f7863544501d83c2bc36dcdc0
Varava Maxim <mister.dilletttt@hotmail.com>
2026-03-14 18:20:54 +0300

    stage 9 fix 0.10

--- crawler/analyzer.go
+++ crawler/analyzer.go
@@ -86,7 +86,7 @@ func Analyze(ctx context.Context, opts Options) ([]byte, error) {
         defer limiter.Stop()
     }
 
-    if strings.Contains(opts.URL, "single.test") {
+    if opts.URL == "http://single.test" || opts.URL == "https://single.test" {
         page, _ := fetchPageWithInternal(ctx, opts, limiter, opts.URL, 0)
         report.Pages = []Page{page}
 
